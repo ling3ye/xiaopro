4d20aa2c24428425090fb3c9af9b222091c9e753
ling3ye <ling3ye@outlook.com>
2026-02-12 21:46:06 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // 将此处替换为你自己的购买的域名，注意要带上 https://
-  site: 'https://ling3ye.github.io/', 
+  site: 'https://ling3ye.github.io/xiaopro/', 
   
   // 绑定自定义域名通常不需要设置 base，除非你要部署在该域名的子目录下
 });
